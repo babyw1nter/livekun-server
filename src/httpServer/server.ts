0a077e563b62e6eb0df326f948dc56ca10b06d1c
hhui64 <907322015@qq.com>
2021-08-26 10:23:54 +0800

    feat(httpServer/server): 新增多种操作接口的鉴权功能

--- src/httpServer/server.ts
+++ src/httpServer/server.ts
@@ -3,10 +3,11 @@ import { wrap, socketServer, sendToProtocol } from '../socketServer/server'
 import path from 'path'
 import consola from 'consola'
 import ConfigManager, { IConfig } from '../configManager'
-import { getRandomChatMessage, getRandomGiftCapsule, getRandomGiftCard } from '../mock'
+import { getRandomChatMessage, getRandomGiftCapsule, getRandomGiftCard, user } from '../mock'
 import { Server } from 'http'
-import CCLinkJSManager, { CCLinkJSInstance } from '../cclinkjsManager'
+import CCLinkJSManager from '../cclinkjsManager'
 import session from 'express-session'
+import userRouter from './user'
 
 const httpServerLog = consola.withTag('httpServer')
 
@@ -15,8 +16,6 @@ const port = 39074
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
 app.use(
   session({
     secret: 'hhui64',
@@ -29,6 +28,8 @@ app.use(
     },
   })
 )
+app.all('*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin)
   res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept,X-Requested-With')
   res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
   res.header('Access-Control-Allow-Credentials', 'true')
@@ -36,96 +37,171 @@ app.use(
   if (req.method === 'OPTIONS') res.sendStatus(200)
   else next()
 })
-
+app.use('/user', userRouter)
 app.use('/', express.static(path.join(__dirname, '../../', 'web')))
 
-const uuid = CCLinkJSManager.createCCLinkJS().uuid
-const cclinkjsInstance = CCLinkJSManager.getCCLinkJSInstance(uuid) as CCLinkJSInstance
+app.get('/api/get-status', (req, res) => {
+  const uuid = req.session.user?.uuid || (req.query.uuid as string)
+  const instance = CCLinkJSManager.getCCLinkJSInstance(uuid)
 
-app.get('/get-config', (req, res) => {
-  ConfigManager.readConfig()
-  res.send({
-    code: 200,
-    data: ConfigManager.getConfig(),
-  })
+  if (uuid !== '' && instance) {
+    res.send({
+      code: 200,
+      data: instance.getStatus(),
+    })
+  } else {
+    res.send({
+      code: 404,
+      message: 'UUID not found.',
+    })
+  }
 })
 
-app.get('/get-status', (req, res) => {
-  res.send({
-    code: 200,
-    data: cclinkjsInstance.getStatus(),
-  })
+app.get('/user/get-config', (req, res) => {
+  const uuid = req.session.user?.uuid || (req.query.uuid as string)
+
+  if (uuid !== '') {
+    res.send({
+      code: 200,
+      data: {
+        uuid,
+        ...ConfigManager.getConfig(uuid),
+      },
+    })
+  } else {
+    res.send({
+      code: 404,
+      message: 'UUID not found.',
+    })
+  }
 })
 
-app.post('/update-config', (req, res) => {
+app.post('/user/update-config', (req, res) => {
+  if (!req.session.user) {
+    res.send({
+      code: 530,
+      message: 'Not logged in.',
+    })
+    return
+  }
+
+  const uuid = req.session.user.uuid as string
+
   ConfigManager.setConfig(req.body as IConfig)
-  ConfigManager.saveConfig()
-  ConfigManager.readConfig()
+  ConfigManager.saveConfig(uuid)
+  ConfigManager.readConfig(uuid)
 
   if (socketServer) {
     sendToProtocol(JSON.stringify(wrap({ type: 'update-config', data: {} })))
     res.send({
       code: 200,
-      data: ConfigManager.getConfig(),
+      data: ConfigManager.getConfig(uuid),
     })
   } else {
     res.send({
       code: 20001,
-      msg: 'Socket 服务端未初始化！',
+      message: 'Socket 服务端未初始化！',
     })
   }
 })
 
-app.post('/join', async (req, res) => {
+app.post('/api/join', async (req, res) => {
+  if (!req.session.user) {
+    res.send({
+      code: 530,
+      message: 'Not logged in.',
+    })
+    return
+  }
+
+  const uuid = req.session.user.uuid as string
+  const instance = CCLinkJSManager.getCCLinkJSInstance(uuid)
+
+  if (!instance) {
+    res.send({
+      code: 10008,
+      message: 'Instance not found.',
+    })
+    return
+  }
+
   const liveId = (req.body.liveId as string).toString()
 
   if (!req.body.liveId) {
     res.send({
       code: 10003,
-      msg: '直播间ID不能为空',
+      message: '直播间ID不能为空',
     })
   }
 
   httpServerLog.info(uuid, '正在进入房间...', liveId)
 
-  cclinkjsInstance
+  instance
     .joinLiveRoom(uuid, liveId)
     .then(() => {
       res.send({
-        code: 10000,
-        msg: 'ok',
+        code: 200,
+        message: 'ok',
       })
 
-      httpServerLog.success(uuid, '进入房间成功！', cclinkjsInstance.getStatus().roomInfo.title)
+      httpServerLog.success(uuid, '进入房间成功！', instance.getStatus().roomInfo.title)
     })
     .catch((reason: Error) => {
       res.send({
         code: 10001,
-        msg: '进入房间失败！' + reason.message,
+        message: '进入房间失败！' + reason.message,
       })
 
       httpServerLog.error(uuid, '进入房间失败！', reason)
     })
 })
 
-app.post('/leave', (req, res) => {
-  cclinkjsInstance
+app.post('/api/reset', (req, res) => {
+  if (!req.session.user) {
+    res.send({
+      code: 530,
+      message: 'Not logged in.',
+    })
+    return
+  }
+
+  const uuid = req.session.user.uuid as string
+  const instance = CCLinkJSManager.getCCLinkJSInstance(uuid)
+
+  if (!instance) {
+    res.send({
+      code: 10008,
+      message: 'Instance not found.',
+    })
+    return
+  }
+
+  instance
     .reset()
     .then(() => {
       res.send({
-        code: 10000,
-        msg: 'ok',
+        code: 200,
+        message: 'ok',
       })
     })
     .catch((reason: Error) => {
       res.send({
         code: 10001,
-        msg: reason.message,
+        message: reason.message,
       })
     })
 })
 
-app.post('/control', (req, res) => {
+app.post('/api/control', (req, res) => {
+  if (!req.session.user) {
+    res.send({
+      code: 530,
+      message: 'Not logged in.',
+    })
+    return
+  }
+
+  const uuid = req.session.user.uuid as string
   const method = req.body.method as string
 
   switch (method) {
@@ -137,7 +213,8 @@ app.post('/control', (req, res) => {
             data: getRandomGiftCapsule(),
           })
         ),
-        'gift-capsule'
+        `gift-capsule`,
+        uuid
       )
       break
     case 'sendMockDataToChatMessage':
@@ -148,7 +225,8 @@ app.post('/control', (req, res) => {
             data: getRandomChatMessage(),
           })
         ),
-        'chat-message'
+        `chat-message`,
+        uuid
       )
       break
     case 'sendMockDataToGiftCard':
@@ -159,23 +237,24 @@ app.post('/control', (req, res) => {
             data: getRandomGiftCard(),
           })
         ),
-        'gift-card'
+        `gift-card`,
+        uuid
       )
       break
     case 'clearGiftCapsule':
-      sendToProtocol(JSON.stringify(wrap({ type: 'clear', data: {} })), 'gift-capsule')
+      sendToProtocol(JSON.stringify(wrap({ type: 'clear', data: {} })), `gift-capsule`, uuid)
       break
     case 'clearChatMessage':
-      sendToProtocol(JSON.stringify(wrap({ type: 'clear', data: {} })), 'chat-message')
+      sendToProtocol(JSON.stringify(wrap({ type: 'clear', data: {} })), `chat-message`, uuid)
       break
     case 'clearGiftCard':
-      sendToProtocol(JSON.stringify(wrap({ type: 'clear', data: {} })), 'gift-card')
+      sendToProtocol(JSON.stringify(wrap({ type: 'clear', data: {} })), `gift-card`, uuid)
       break
   }
 
   res.send({
     code: 200,
-    msg: 'ok',
+    message: 'ok',
   })
 })
 
