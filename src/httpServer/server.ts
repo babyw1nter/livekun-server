1a4621769fc05b3a805841ba0135ecb6c34f0424
hhui64 <907322015@qq.com>
2021-08-28 01:42:52 +0800

    fix(httpServer/server): 修复因响应头类型错误引起的前端页面无法加载的问题

--- src/httpServer/server.ts
+++ src/httpServer/server.ts
@@ -41,7 +41,7 @@ app.all('*', (req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept, X-Requested-With')
   res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
   res.header('Access-Control-Allow-Credentials', 'true')
-  res.header('Content-Type', 'application/json; charset=utf-8')
+  // res.header('Content-Type', 'application/json; charset=utf-8')
   res.header('X-Powered-By', 'Livekun Server')
   if (req.method === 'OPTIONS') res.sendStatus(200)
   else next()
