59f1bfb99338699b71c06536396b87559df61ec2
hhui64 <907322015@qq.com>
2021-08-28 00:17:46 +0800

    perf(httpServer/server): 优化响应头的配置

--- src/httpServer/server.ts
+++ src/httpServer/server.ts
@@ -39,10 +39,11 @@ app.use(
 )
 app.all('*', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', req.headers.origin)
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept,X-Requested-With')
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept, X-Requested-With')
   res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
   res.header('Access-Control-Allow-Credentials', 'true')
-  res.header('X-Powered-By', ' 3.2.1')
+  res.header('Content-Type', 'application/json; charset=utf-8')
+  res.header('X-Powered-By', 'Livekun Server')
   if (req.method === 'OPTIONS') res.sendStatus(200)
   else next()
 })
