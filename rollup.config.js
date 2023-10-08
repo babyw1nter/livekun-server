36a573b00158381b96169c8179e293d7ee044670
hhui64 <907322015@qq.com>
2023-10-08 17:47:47 +0800

    style: lint

--- rollup.config.js
+++ rollup.config.js
@@ -11,21 +11,21 @@ const extensions = ['.js', '.ts', '.tsx']
 // ts
 const tsPlugin = ts({
   tsconfig: getPath('./tsconfig.json'), // 导入本地ts配置
-  extensions,
+  extensions
 })
 
 // 基础配置
 const commonConf = {
   input: getPath('./src/index.ts'),
-  plugins: [tsPlugin, resolve(extensions), commonjs({ include: /node_modules/ }), json()],
+  plugins: [tsPlugin, resolve(extensions), commonjs({ include: /node_modules/ }), json()]
 }
 
 // 需要导出的模块类型
 const outputMap = [
   {
     file: packageJSON.module, // es6模块
-    format: 'es',
-  },
+    format: 'es'
+  }
 ]
 
 const buildConf = (options) => Object.assign({}, commonConf, options)
