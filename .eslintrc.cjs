e6ba4123c7eb43619f925ee5c6c636f6627ade4d
hhui64 <907322015@qq.com>
2023-10-08 16:16:48 +0800

    chore(eslint): ignore

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,6 +16,7 @@ module.exports = {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
   },
+  ignorePatterns: ['dist/*', 'config/*', 'data/*'],
   rules: {
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
