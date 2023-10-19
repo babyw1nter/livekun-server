f77398473497f6a820a0e6d3a76f8d81109f0e4d
hhui64 <907322015@qq.com>
2023-10-19 08:33:04 +0800

    chore: .gitignore

---
+++ config/default.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  server: {
+    host: 'localhost',
+    port: 39074
+  },
+  redis: {
+    host: '127.0.0.1',
+    port: 6379,
+    username: '',
+    password: ''
+  },
+  session: {
+    cookie: {
+      secure: false,
+      domain: '',
+      httpOnly: true
+    }
+  }
+}
