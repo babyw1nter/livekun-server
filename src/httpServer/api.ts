1c2b721ec44afa2f3ad5aabcb98dafc1d27ade82
hhui64 <907322015@qq.com>
2023-10-23 12:18:03 +0800

    feat: 从 `express` 迁移至 `fastify`

--- src/httpServer/api.ts
+++ src/httpServer/api.ts
@@ -1,200 +1,212 @@
 import { send } from '../socketServer/server'
 import consola from 'consola'
-import express from 'express'
 import CCLinkJSManager from '../cclinkjsManager'
 import { resWrap } from './server'
 import { readFileSync } from 'node:fs'
 import path, { resolve } from 'node:path'
 import { getRandomTicket, getRandomChatMessage, getRandomPaid, mockData } from '../mock'
 import { PluginNames, PluginActions } from '../api/plugins'
+import { FastifyInstance } from 'fastify/types/instance'
 
 const log = consola.withTag('httpserver/api')
 
-const apiRouter = express.Router()
+export default async function (fastify: FastifyInstance) {
+  fastify.get('/getBroadcasts', (request, reply) => {
+    interface Broadcasts {
+      banner: string
+      broadcastsToptips: string
+      broadcasts: Array<string>
+    }
 
-apiRouter.get('/getBroadcasts', (req, res) => {
-  interface Broadcasts {
-    banner: string
-    broadcastsToptips: string
-    broadcasts: Array<string>
-  }
+    try {
+      const broadcasts = JSON.parse(
+        readFileSync(path.join(resolve(), '/data/broadcasts.json')).toString()
+      ) as Broadcasts
 
-  try {
-    const broadcasts = JSON.parse(readFileSync(path.join(resolve(), '/data/broadcasts.json')).toString()) as Broadcasts
+      reply.send(
+        resWrap(200, 'ok', {
+          banner: broadcasts.banner,
+          broadcastsToptips: broadcasts.broadcastsToptips,
+          broadcasts: broadcasts.broadcasts
+        })
+      )
+    } catch (error: unknown) {
+      reply.send(resWrap(20001, '获取公告失败！'))
+    }
+  })
 
-    res.json(
+  fastify.get('/getMockdata', (request, reply) => {
+    reply.send(
       resWrap(200, 'ok', {
-        banner: broadcasts.banner,
-        broadcastsToptips: broadcasts.broadcastsToptips,
-        broadcasts: broadcasts.broadcasts
+        ticketMockData: mockData.ticketList,
+        chatMessageMockData: mockData.chatMessageList,
+        paidMockData: mockData.paidList
       })
     )
-  } catch (error: unknown) {
-    res.json(resWrap(20001, '获取公告失败！'))
-  }
-})
+  })
 
-apiRouter.get('/getMockdata', (req, res) => {
-  res.json(
-    resWrap(200, 'ok', {
-      ticketMockData: mockData.ticketList,
-      chatMessageMockData: mockData.chatMessageList,
-      paidMockData: mockData.paidList
-    })
-  )
-})
+  fastify.get<{
+    Querystring: {
+      uuid: string
+    }
+  }>('/getStatus', (request, reply) => {
+    const uuid = request.session.user?.uuid || request.query.uuid
+    const instance = CCLinkJSManager.getCCLinkJSInstance(uuid)
 
-apiRouter.get('/getStatus', (req, res) => {
-  const uuid = req.session.user?.uuid || (req.query.uuid as string)
-  const instance = CCLinkJSManager.getCCLinkJSInstance(uuid)
+    // 这里因为是公开接口，UUID 并不确定存在 session 中
+    // 所以当实例不存在时直接返回错误消息就可以了
+    if (uuid !== '' && instance) {
+      reply.send(resWrap(200, 'ok', instance.getStatus()))
+    } else {
+      reply.send(resWrap(10008, 'Instance not found.'))
+    }
+  })
 
-  // 这里因为是公开接口，UUID 并不确定存在 session 中
-  // 所以当实例不存在时直接返回错误消息就可以了
-  if (uuid !== '' && instance) {
-    res.json(resWrap(200, 'ok', instance.getStatus()))
-  } else {
-    res.json(resWrap(10008, 'Instance not found.'))
-  }
-})
+  fastify.post<{
+    Body: {
+      liveId: string
+    }
+  }>('/join', async (request, reply) => {
+    const uuid = request.session.user?.uuid
+    const liveId = request.body.liveId.toString()
 
-apiRouter.post('/join', async (req, res) => {
-  const uuid = req.session.user?.uuid as string
-  const liveId = (req.body.liveId as string).toString()
+    if (!liveId) {
+      reply.send(resWrap(10003, '未指定直播间ID'))
+      return
+    }
 
-  if (!liveId) {
-    res.json(resWrap(10003, '未指定直播间ID'))
-    return
-  }
+    const instance = CCLinkJSManager.getCCLinkJSInstance(uuid) || CCLinkJSManager.createCCLinkJS(uuid)
 
-  const instance = CCLinkJSManager.getCCLinkJSInstance(uuid) || CCLinkJSManager.createCCLinkJS(uuid)
+    log.info(uuid, '正在进入房间...', liveId)
 
-  log.info(uuid, '正在进入房间...', liveId)
+    const join = () => {
+      instance
+        .joinLiveRoom(uuid, liveId)
+        .then(() => {
+          reply.send(
+            resWrap(200, 'ok', {
+              status: instance.getStatus()
+            })
+          )
+          log.success(uuid, '进入房间成功！', instance.getStatus().roomInfo.title)
+        })
+        .catch((reason: Error) => {
+          reply.send(
+            resWrap(10001, '进入房间失败！' + reason.message, {
+              status: instance.getStatus()
+            })
+          )
+          log.error(uuid, '进入房间失败！', reason)
+        })
+    }
 
-  const join = () => {
-    instance
-      .joinLiveRoom(uuid, liveId)
-      .then(() => {
-        res.json(
-          resWrap(200, 'ok', {
-            status: instance.getStatus()
-          })
-        )
-        log.success(uuid, '进入房间成功！', instance.getStatus().roomInfo.title)
-      })
-      .catch((reason: Error) => {
-        res.json(
-          resWrap(10001, '进入房间失败！' + reason.message, {
-            status: instance.getStatus()
-          })
-        )
-        log.error(uuid, '进入房间失败！', reason)
-      })
-  }
+    // 实例未就绪时，直接执行 reset 方法重置实例，待重置完成后再执行后续操作
+    if (!instance.cclinkjs.isReady()) {
+      instance
+        .reset()
+        .then(() => join())
+        .catch((reason: Error) => {
+          reply.send(
+            resWrap(10004, '进入房间失败，等待实例就绪错误！' + reason.message, {
+              status: instance.getStatus()
+            })
+          )
+        })
+    } else {
+      join()
+    }
+  })
 
-  // 实例未就绪时，直接执行 reset 方法重置实例，待重置完成后再执行后续操作
-  if (!instance.cclinkjs.isReady()) {
+  fastify.post('/reset', (request, reply) => {
+    const uuid = request.session.user?.uuid
+    const instance = CCLinkJSManager.getCCLinkJSInstance(uuid) || CCLinkJSManager.createCCLinkJS(uuid)
+
     instance
       .reset()
-      .then(() => join())
+      .then(() => {
+        reply.send(
+          resWrap(200, 'ok', {
+            instatus: instance.getStatus()
+          })
+        )
+      })
       .catch((reason: Error) => {
-        res.json(
-          resWrap(10004, '进入房间失败，等待实例就绪错误！' + reason.message, {
+        reply.send(
+          resWrap(30001, '重置失败！' + reason.message, {
             status: instance.getStatus()
           })
         )
       })
-  } else {
-    join()
-  }
-})
+  })
 
-apiRouter.post('/reset', (req, res) => {
-  const uuid = req.session.user?.uuid as string
-  const instance = CCLinkJSManager.getCCLinkJSInstance(uuid) || CCLinkJSManager.createCCLinkJS(uuid)
+  fastify.post<{
+    Body: {
+      method: string
+    }
+  }>('/control', (request, reply) => {
+    const uuid = request.session.user?.uuid
+    const method = request.body.method
 
-  instance
-    .reset()
-    .then(() => {
-      res.json(
-        resWrap(200, 'ok', {
-          instatus: instance.getStatus()
-        })
-      )
-    })
-    .catch((reason: Error) => {
-      res.json(
-        resWrap(30001, '重置失败！' + reason.message, {
-          status: instance.getStatus()
-        })
-      )
-    })
-})
+    switch (method) {
+      case 'sendMockDataToChatMessage':
+        send(
+          {
+            type: 'PLUGIN_MESSAGE',
+            data: getRandomChatMessage()
+          },
+          PluginNames.PLUGIN_CHAT_MESSAGE,
+          uuid
+        )
+        break
+      case 'sendMockDataToTicket':
+        send(
+          {
+            type: 'PLUGIN_MESSAGE',
+            data: getRandomTicket()
+          },
+          PluginNames.PLUGIN_TICKET,
+          uuid
+        )
+        break
+      case 'sendMockDataToPaid':
+        send(
+          {
+            type: 'PLUGIN_MESSAGE',
+            data: getRandomPaid()
+          },
+          PluginNames.PLUGIN_PAID,
+          uuid
+        )
+        break
+      case 'clearChatMessage':
+        send({ type: 'PLUGIN_ACTION', data: { action: PluginActions.CLEAR } }, PluginNames.PLUGIN_CHAT_MESSAGE, uuid)
+        break
+      case 'clearTicket':
+        send({ type: 'PLUGIN_ACTION', data: { action: PluginActions.CLEAR } }, PluginNames.PLUGIN_TICKET, uuid)
+        break
+      case 'clearPaid':
+        send({ type: 'PLUGIN_ACTION', data: { action: PluginActions.CLEAR } }, PluginNames.PLUGIN_PAID, uuid)
+        break
+      case 'refreshChatMessage':
+        send(
+          { type: 'PLUGIN_ACTION', data: { action: PluginActions.REFRESH_PAGE } },
+          PluginNames.PLUGIN_CHAT_MESSAGE,
+          uuid
+        )
+        break
+      case 'refreshTicket':
+        send({ type: 'PLUGIN_ACTION', data: { action: PluginActions.REFRESH_PAGE } }, PluginNames.PLUGIN_TICKET, uuid)
+        break
+      case 'refreshPaid':
+        send({ type: 'PLUGIN_ACTION', data: { action: PluginActions.REFRESH_PAGE } }, PluginNames.PLUGIN_PAID, uuid)
+        break
+    }
 
-apiRouter.post('/control', (req, res) => {
-  const uuid = req.session.user?.uuid as string
-  const method = req.body.method as string
-
-  switch (method) {
-    case 'sendMockDataToChatMessage':
-      send(
-        {
-          type: 'PLUGIN_MESSAGE',
-          data: getRandomChatMessage()
-        },
-        PluginNames.PLUGIN_CHAT_MESSAGE,
-        uuid
-      )
-      break
-    case 'sendMockDataToTicket':
-      send(
-        {
-          type: 'PLUGIN_MESSAGE',
-          data: getRandomTicket()
-        },
-        PluginNames.PLUGIN_TICKET,
-        uuid
-      )
-      break
-    case 'sendMockDataToPaid':
-      send(
-        {
-          type: 'PLUGIN_MESSAGE',
-          data: getRandomPaid()
-        },
-        PluginNames.PLUGIN_PAID,
-        uuid
-      )
-      break
-    case 'clearChatMessage':
-      send({ type: 'PLUGIN_ACTION', data: { action: PluginActions.CLEAR } }, PluginNames.PLUGIN_CHAT_MESSAGE, uuid)
-      break
-    case 'clearTicket':
-      send({ type: 'PLUGIN_ACTION', data: { action: PluginActions.CLEAR } }, PluginNames.PLUGIN_TICKET, uuid)
-      break
-    case 'clearPaid':
-      send({ type: 'PLUGIN_ACTION', data: { action: PluginActions.CLEAR } }, PluginNames.PLUGIN_PAID, uuid)
-      break
-    case 'refreshChatMessage':
-      send(
-        { type: 'PLUGIN_ACTION', data: { action: PluginActions.REFRESH_PAGE } },
-        PluginNames.PLUGIN_CHAT_MESSAGE,
-        uuid
-      )
-      break
-    case 'refreshTicket':
-      send({ type: 'PLUGIN_ACTION', data: { action: PluginActions.REFRESH_PAGE } }, PluginNames.PLUGIN_TICKET, uuid)
-      break
-    case 'refreshPaid':
-      send({ type: 'PLUGIN_ACTION', data: { action: PluginActions.REFRESH_PAGE } }, PluginNames.PLUGIN_PAID, uuid)
-      break
-  }
-
-  res.json(
-    resWrap(200, 'ok', {
-      uuid,
-      method
-    })
-  )
-})
-
-export default apiRouter
+    reply.send(
+      resWrap(200, 'ok', {
+        uuid,
+        method
+      })
+    )
+  })
+}
--- src/httpServer/user.ts
+++ src/httpServer/user.ts
@@ -1,159 +1,134 @@
-import express from 'express'
 import CCLinkJSManager from '../cclinkjsManager'
-import UserConfigManager, { IUserPluginsConfig } from '../UserConfigManager'
-import { socketServer, send } from '../socketServer/server'
+import UserConfigManager from '../UserConfigManager'
+import { send } from '../socketServer/server'
 import { resWrap } from './server'
-import { readFileSync } from 'node:fs'
-import path, { resolve } from 'node:path'
-import { PluginNames, PluginActions } from '../api/plugins'
+import { PluginNames, PluginActions, IPluginConfigMap } from '../api/plugins'
+import { FastifyInstance } from 'fastify/types/instance'
+import { UserManager } from '../UserManager'
 
-interface IUser {
-  uuid: string
-  username: string
-  password: string
-  isBan: boolean
-}
-
-interface UserData {
-  users: Array<IUser>
-}
-
-class UserManager {
-  static userData: UserData
-
-  static read(): void {
-    try {
-      UserManager.userData = JSON.parse(readFileSync(path.join(resolve(), 'data', 'user', 'users.json')).toString())
-    } catch (error) {
-      UserManager.userData = {
-        users: []
-      }
+export default async function (fastify: FastifyInstance) {
+  fastify.post<{
+    Body: {
+      username: string
+      password: string
+      autologin: boolean
     }
-  }
+  }>('/login', (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+    const autologin = req.body.autologin
 
-  static login(username: string, password: string): IUser | null {
-    UserManager.read()
+    if (!username || !password) {
+      res.send(resWrap(10002, '参数不正确'))
+      return
+    }
 
-    const user = UserManager.userData.users.find((u) => u.username === username)
+    const user = UserManager.login(username, password)
 
     if (user) {
-      if (user.username === username && user.password === password) {
-        return user
+      // 设置 session
+      if (autologin) {
+        req.session.cookie.maxAge = 1296000000
       }
+
+      req.session.user = {
+        username: user.username,
+        uuid: user.uuid
+      }
+
+      // 登陆成功时，为其创建 cclinkjs 实例
+      const instance = CCLinkJSManager.createCCLinkJS(user.uuid)
+
+      res.send(resWrap(200, '登录成功', req.session.user))
+    } else {
+      res.send(resWrap(10001, '用户名或密码不正确'))
+      return
+    }
+  })
+
+  fastify.post('/autologin', (req, res) => {
+    const uuid = req.session.user?.uuid
+
+    let instance = CCLinkJSManager.getCCLinkJSInstance(uuid)
+
+    if (!instance) {
+      instance = CCLinkJSManager.createCCLinkJS(uuid)
     }
 
-    return null
-  }
-}
+    res.send(resWrap(200, 'session 登录成功', req.session.user))
+  })
 
-const userRouter = express.Router()
+  fastify.get('/logout', (req, res) => {
+    // const uuid = req.session.user?.uuid
+    // CCLinkJSManager.destroyCCLinkJSInstance(uuid)
 
-userRouter.post('/login', (req, res) => {
-  const username = req.body.username as string
-  const password = req.body.password as string
-  const autologin = req.body.autologin as boolean
+    req.session.destroy(() => null)
+    res.send(resWrap(200, '注销成功'))
+  })
 
-  if (!username || !password) {
-    res.json(resWrap(10002, '参数不正确'))
-    return
-  }
-
-  const user = UserManager.login(username, password)
-
-  if (user) {
-    // 设置 session
-    if (autologin) {
-      req.session.cookie.maxAge = 1296000000
+  fastify.get<{
+    Querystring: {
+      uuid: string
+      pluginName: string
     }
+  }>('/getPluginConfig', (req, res) => {
+    const uuid = req.session.user?.uuid || req.query.uuid || ''
+    const pluginName = req.query.pluginName
+      .split(',')
+      .map((i) => i.trim())
+      .filter((i) => i !== '')
 
-    req.session.user = {
-      username: user.username,
-      uuid: user.uuid
+    if (uuid !== '' && uuid.length < 64) {
+      const userConfig = UserConfigManager.get(uuid)
+      const pluginConfig = userConfig.getPluginConfig(pluginName)
+
+      res.send(resWrap(200, 'ok', pluginConfig))
+    } else {
+      res.send(resWrap(404, 'UUID not found.'))
     }
+  })
 
-    // 登陆成功时，为其创建 cclinkjs 实例
-    const instance = CCLinkJSManager.createCCLinkJS(user.uuid)
+  fastify.post<{
+    Body: {
+      pluginName: PluginNames
+      pluginConfig: IPluginConfigMap[PluginNames]
+    }
+  }>('/setPluginConfig', (req, res) => {
+    const uuid = req.session.user?.uuid
 
-    res.json(resWrap(200, '登录成功', req.session.user))
-  } else {
-    res.json(resWrap(10001, '用户名或密码不正确'))
-    return
-  }
-})
+    const pluginName = req.body.pluginName
+    const pluginConfig = req.body.pluginConfig
 
-userRouter.post('/autologin', (req, res) => {
-  const uuid = req.session.user?.uuid as string
-
-  let instance = CCLinkJSManager.getCCLinkJSInstance(uuid)
-
-  if (!instance) {
-    instance = CCLinkJSManager.createCCLinkJS(uuid)
-  }
-
-  res.json(resWrap(200, 'session 登录成功', req.session.user))
-})
-
-userRouter.get('/logout', (req, res) => {
-  // const uuid = req.session.user?.uuid as string
-  // CCLinkJSManager.destroyCCLinkJSInstance(uuid)
-
-  req.session.destroy(() => null)
-  res.json(resWrap(200, '注销成功'))
-})
-
-userRouter.get('/getPluginConfig', (req, res) => {
-  const uuid = req.session.user?.uuid || (req.query.uuid as string) || ''
-  const pluginName = (req.query.pluginName as string)
-    .split(',')
-    .map((i) => i.trim())
-    .filter((i) => i !== '')
-
-  if (uuid !== '' && uuid.length < 64) {
     const userConfig = UserConfigManager.get(uuid)
-    const pluginConfig = userConfig.getPluginConfig(pluginName)
 
-    res.json(resWrap(200, 'ok', pluginConfig))
-  } else {
-    res.json(resWrap(404, 'UUID not found.'))
-  }
-})
+    userConfig.setPluginConfig(pluginName, pluginConfig)
 
-userRouter.post('/setPluginConfig', (req, res) => {
-  const uuid = req.session.user?.uuid as string
+    userConfig.save().read()
 
-  const pluginName = req.body.pluginName as PluginNames
-  const pluginConfig = req.body.pluginConfig
-
-  const userConfig = UserConfigManager.get(uuid)
-
-  userConfig.setPluginConfig(pluginName, pluginConfig)
-
-  userConfig.save().read()
-
-  // 向正在连接的插件下发更新配置请求
-  if (socketServer) {
+    // 向正在连接的插件下发更新配置请求
     send({ type: 'PLUGIN_ACTION', data: { action: PluginActions.REFRESH_CONFIG } }, pluginName, uuid)
-  }
 
-  res.json(resWrap(200, 'ok'))
-})
+    res.send(resWrap(200, 'ok'))
+  })
 
-userRouter.post('/resetPluginConfig', (req, res) => {
-  const uuid = req.session.user?.uuid as string
-  const pluginName = req.body.pluginName as PluginNames
+  fastify.post<{
+    Body: {
+      pluginName: PluginNames
+    }
+  }>('/resetPluginConfig', (req, res) => {
+    const uuid = req.session.user?.uuid
+    const pluginName = req.body.pluginName
 
-  const userConfig = UserConfigManager.get(uuid)
+    const userConfig = UserConfigManager.get(uuid)
 
-  userConfig.resetPluginConfig(pluginName)
+    userConfig.resetPluginConfig(pluginName)
 
-  userConfig.save().read()
+    userConfig.save().read()
 
-  // 向正在连接的插件下发更新配置请求
-  if (socketServer) {
+    // 向正在连接的插件下发更新配置请求
+
     send({ type: 'PLUGIN_ACTION', data: { action: PluginActions.REFRESH_CONFIG } }, pluginName, uuid)
-  }
 
-  res.json(resWrap(200, 'ok'))
-})
-
-export default userRouter
+    res.send(resWrap(200, 'ok'))
+  })
+}
